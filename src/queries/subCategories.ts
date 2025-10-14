e4d1c1e3b95a22d8506e3175cde297b10695f99a
Amin-Chogueur <chogueuramine@gmail.com>
2025-10-14 21:52:53 +0100

    edit the url for fetching  categories

--- src/queries/subCategories.ts
+++ src/queries/subCategories.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 export async function fetchCategories(category: string | null) {
   try {
-    const res = await axios.get(`${BASE_URL}/api/categories`, {
+    const res = await axios.get(`${BASE_URL}/api/categories/client`, {
       params: {
         category,
       },
